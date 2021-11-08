0fa9d2fa113024825aa956b1a449bc60af18f400
Jarae-MacyMan <sjaraebr@gmail.com>
2021-11-08 15:05:40 +0000

    done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 //write your code here
+const averageInArray = function(aRR){
+  let sum = 0; 
+  for (let i = 0; i < aRR.length; i++){
+    sum += aRR[i];
+  }
+  let avg = (sum/aRR.length);
+  return avg;
+};
+
+console.log(averageInArray([2, 2, 5, 10]));
